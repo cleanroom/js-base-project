645f0583f1672c7e41b5489ccb7cf940426c47ac
Seaborn Lee <seabornlee.cn@gmail.com>
2017-08-25 20:00:02 +0800

    Complete

--- spec/main_spec.js
+++ spec/main_spec.js
@@ -1,31 +1,39 @@
 "use strict";
-var _ = require("lodash");
 var chai = require("chai");
-var sinon = require("sinon");
-var sinonChai = require("sinon-chai");
 var expect = chai.expect;
-chai.use(sinonChai);
 
-var main = require("../lib/main.js");
+describe("map reduce", function(){
+    before(function () {
+        Array.prototype.reduce = function(fn, initialValue) {
+            var result = initialValue;
+            for (var i in this) {
+                result = fn(result, this[i]);
+            }
+            return result;
+        };
 
-
-describe("测试描述", function(){
-    sinon.spy(console, 'log');
-
-    it("测试用例1", function(){
-
-        var result = main();
-        var expect_string = '';
-        
-        expect(expect_string).to.equal(result);
+        Array.prototype.map = function (fn) {
+            return this.reduce(function(result, item) {
+                result.push(fn(item));
+                return result;
+            }, []);
+        }
     });
 
-    it("测试用例2", function(){
+    it("should reduce", function () {
+        var arr = [1, 2, 3];
+        var result = arr.reduce(function(sum, number) {
+            return sum + number;
+        }, 0);
+        expect(result).to.equal(6);
+    });
 
-        main();
-        var result = _.flatten(console.log.args).join("\n");
-        var expect_string = '';
+    it("should map", function(){
+        var arr = [1, 2, 3];
+        var result = arr.map(function (number) {
+            return number * 2;
+        });
 
-        expect(expect_string).to.equal(result);
+        expect(result).to.deep.equal([2, 4, 6]);
     });
 });
